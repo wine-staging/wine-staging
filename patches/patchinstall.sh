4b6879f30fc7b9af063f479077bdc211b28e32b2
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2021-08-14 17:18:31 +1000

    Release v6.15

--- patches/patchinstall.sh
+++ patches/patchinstall.sh
@@ -57,7 +57,7 @@ upstream_commit()
 # Show version information
 version()
 {
-	echo "Wine Staging 6.14"
+	echo "Wine Staging 6.15"
 	echo "Copyright (C) 2014-2019 the Wine Staging project authors."
 	echo "Copyright (C) 2018-2020 Alistair Leslie-Hughes"
 	echo ""
