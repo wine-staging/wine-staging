5d30a5655f83424cefe9c084e682af6a09da830d
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2022-03-22 10:46:29 +1100

    Rebase against 429325b6891bbb7488b3e8d8f97e4abb92879ce7.

--- patches/patchinstall.sh
+++ patches/patchinstall.sh
@@ -51,7 +51,7 @@ usage()
 # Get the upstream commit sha
 upstream_commit()
 {
-	echo "47b02e8c1ea4ad82cd572dc3dcf60af753222f39"
+	echo "429325b6891bbb7488b3e8d8f97e4abb92879ce7"
 }
 
 # Show version information
